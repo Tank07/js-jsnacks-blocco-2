9a45fc04f0fa8f536ffbd832132a17e177902737
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-16 18:15:02 +0100

    bozza snack 2

--- snack-2/main.js
+++ snack-2/main.js
@@ -5,8 +5,8 @@
 // console.log(parolaDue, parolaDue.length)
 
 // if (parolaUno.length > parolaDue.length){
-//     console.log(parolaUno, parolaDue)
-//     document.getElementById("esito").innerHTML = `<div>La prima parola inserita: <span class="blue">"${parolaUno}"</span> è la più lunga</span> con ben ${parolaUno.length} lettere rispetto alla seconda parola inserita: <span class="blue">"${parolaDue}"</span> che è più corta </span> con ben ${parolaDue.length} lettere</div>`;
+// console.log(parolaUno, parolaDue)
+// document.getElementById("esito").innerHTML = `<div>La prima parola inserita: <span class="blue">"${parolaUno}"</span> è la più lunga</span> con ben ${parolaUno.length} lettere rispetto alla seconda parola inserita: <span class="blue">"${parolaDue}"</span> che è più corta </span> con ben ${parolaDue.length} lettere</div>`;
 // } else if (parolaUno.length < parolaDue.length){
 //     console.log(parolaDue, parolaUno)
 //     document.getElementById("esito").innerHTML = `<div>La seconda parola inserita: <span class="blue">"${parolaDue}"</span> è la più lunga</span> con ben ${parolaDue.length} lettere rispetto alla seconda parola inserita: <span class="blue">"${parolaDue}"</span> che è più corta </span> con ben ${parolaUno.length} lettere</div>`;
@@ -16,3 +16,15 @@
 
 // Snack2
 // Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+
+let nomi = ["Modestino", "Abdelhamid" ,"Andrea","Alberto","Vincenzo","Gerardo" ,"Luca","Tancredi","Valentino","Ludovica","Davide","Nizar","Alessandro","Gabriele" ,"Stefano","Angelo","Caterina","Sergio","Clara","Matteo","Simone" ,"Noemi","Roberto","Andrea","Andrea","Simone" ,"Samuele" ,"Vera","Ivan","Michael","Simone", "Luca","Francesco"];
+console.log(nomi.length)
+
+let cognomi = ["Arpa","Ayadi","Bellucci","Biga","Borriello","Calia","Carbone","Carluccio","DeStefano","DiBenedetto","DiPietro","Fergag","Fonzi","Magni","Maiorchini","Melotti","Moccia","Nifosi","Nocilla","Nuvoloni","Pavone","Pintaldi","Piva","Pontillo","Rosellini","Saputo","Spadaro","Sprinceana","Toselli","Wachnicki","Zaino","Natoli"]
+console.log(cognomi.length)
+
+for (i=1; i<= nomi.length; i++){
+    document.getElementById("esito").innerHTML += `<div> L'invitato numero ${i} per l'esclusiva festa per i Booleani in casa Gatsby è <span class="blue">"${nomi[i]} ${cognomi[i]}"</span></div>`;
+
+}
