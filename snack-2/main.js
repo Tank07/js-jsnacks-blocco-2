f015a8dcffe9bd7b56db664516108a078c9b1149
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-16 23:23:44 +0100

    Finito con aggiunta trash

--- snack-2/main.js
+++ snack-2/main.js
@@ -18,13 +18,16 @@
 // Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
 
-let nomi = ["Modestino", "Abdelhamid" ,"Andrea","Alberto","Vincenzo","Gerardo" ,"Luca","Tancredi","Valentino","Ludovica","Davide","Nizar","Alessandro","Gabriele" ,"Stefano","Angelo","Caterina","Sergio","Clara","Matteo","Simone" ,"Noemi","Roberto","Andrea","Andrea","Simone" ,"Samuele" ,"Vera","Ivan","Michael","Simone", "Luca","Francesco"];
+let nomi = ["Modestino", "Abdelhamid" ,"Andrea","Alberto","Vincenzo","Gerardo" ,"Luca","Tancredi","Valentino","Ludovica","Davide","Nizar","Alessandro","Gabriele" ,"Stefano","Angelo","Caterina","Sergio","Clara","Matteo","Simone" ,"Noemi","Roberto","Andrea","Andrea","Simone" ,"Samuele" ,"Vera","Ivan","Michael","Simone Luca","Francesco"];
 console.log(nomi.length)
 
-let cognomi = ["Arpa","Ayadi","Bellucci","Biga","Borriello","Calia","Carbone","Carluccio","DeStefano","DiBenedetto","DiPietro","Fergag","Fonzi","Magni","Maiorchini","Melotti","Moccia","Nifosi","Nocilla","Nuvoloni","Pavone","Pintaldi","Piva","Pontillo","Rosellini","Saputo","Spadaro","Sprinceana","Toselli","Wachnicki","Zaino","Natoli"]
+let cognomi = ["Arpa","Ayadi","Bellucci","Biga","Borriello","Calia","Carbone","Carluccio","De Stefano","Di Benedetto","Di Pietro","Fergag","Fonzi","Magni","Maiorchini","Melotti","Moccia","Nifosi","Nocilla","Nuvoloni","Pavone","Pintaldi","Piva","Pontillo","Rosellini","Saputo","Spadaro","Sprinceana","Toselli","Wachnicki","Zaino","Natoli"]
 console.log(cognomi.length)
 
-for (i=1; i<= nomi.length; i++){
-    document.getElementById("esito").innerHTML += `<div> L'invitato numero ${i} per l'esclusiva festa per i Booleani in casa Gatsby è <span class="blue">"${nomi[i]} ${cognomi[i]}"</span></div>`;
+for (i=1; i < nomi.length; i++){
+    let randomNome = Math.floor(Math.random()*nomi.length);
+    let randomCognome = Math.floor(Math.random()*cognomi.length);
+    document.getElementById("esito").innerHTML += `<div> <span class="white">L'invitato numero ${i} per l'esclusiva festa per i Booleani in casa Gatsby è </span> <span class="blue">"${nomi[randomNome]} ${cognomi[randomCognome]}"</span></div>`;
 
 }
+
