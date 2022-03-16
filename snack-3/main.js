ba9604db7ee4d9bf6d278bbef38183c9c03709b9
Tancredi Carluccio <tanc_@hotmail.it>
2022-03-17 00:14:55 +0100

    Esercizio 3 e 4 bozza

--- snack-3/main.js
+++ snack-3/main.js
@@ -1,14 +1,21 @@
-// let somma = 0;
-
-
-// for (i=0; i<=10; i++){
-//     let numeri = parseInt(prompt("Inserisci un numero")); 
-//     somma += numeri;
-//     console.log(somma,)
-// }
-
-     
-//     document.getElementById("esito").innerHTML = `<div>La somma di tutti i numeri inseriti è <span class="blue">"${somma}"</span> </div>`;    
-
 // Snack3
 // Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
+
+
+let arrayCreato = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+console.log(arrayCreato);
+
+let somma = 0;
+
+for (i=0; i<=arrayCreato.lenght; i++){
+    console.log(i);
+    if (i % 2 !== 0){
+        console.log(i);
+        somma += arrayCreato[i];   
+        console.log(somma,arrayCreato[i])     
+    }
+
+}
+
+document.getElementById("esito").innerHTML = `<div>La somma di tutti i numeri in posizione dispari è <span class="blue">"${somma}"</span> </div>`;    
+
--- snack-4/main.js
+++ snack-4/main.js
@@ -1,2 +1,24 @@
 // Snack4 (Bonus)
 // Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+
+let arrayUno = [1,2,3,4,5,6,7,8,9,10];
+
+let arrayDue = [1,2,3];
+
+console.log(arrayUno.lenght,arrayDue.length)
+
+for (i=0; i<1000; i++){
+
+    if(arrayUno.lenght < arrayDue.lenght){
+        arrayUno.push(Math.floor(Math.random()* 10))
+
+    } else if (arrayUno.lenght > arrayDue.lenght){
+        arrayDue.push(Math.floor(Math.random()* 10))
+
+    } else if (arrayUno.lenght == arrayDue.lenght){
+        console.log(arrayUno.lenght,arrayDue.length)
+        {break; }    
+    }
+
+}
